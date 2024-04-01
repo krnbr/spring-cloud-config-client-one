0348c5b6bc64eb02cb4ae1c0ff761ac2dde71d10
Karanbir Singh <karanbir.singh@team.telstra.com>
2024-04-01 20:22:23 +0530

    init

---
+++ src/main/java/in/neuw/config/client/SpringCloudConfigClientOneApplication.java
@@ -0,0 +1,13 @@
+package in.neuw.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientOneApplication.class, args);
+    }
+
+}
